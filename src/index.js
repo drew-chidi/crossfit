2d8378a1087509a71607aa767c6bbd36426d857f
drew-chidi <chidi.andrew@gmail.com>
2023-03-01 23:12:30 +0100

    added documentation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const express = require("express");
-
 const bodyParser = require("body-parser");
+// *** ADD ***
+const apicache = require("apicache");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
 const app = express();
+// *** ADD ***
+const cache = apicache.middleware;
 const PORT = process.env.PORT || 3000;
 
-// For testing purposes
 app.use(bodyParser.json());
+// *** ADD ***
+app.use(cache("2 minutes"));
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
+  V1SwaggerDocs(app, PORT);
 });
